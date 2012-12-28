8214b800950a4dc3dacf8e96dacabaefd2c5e7cf
Maxime <mmarinel@efidev.com>
2012-12-28 17:49:50 +0100

    fix namespace again -_-

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SphinxBundle\DependencyInjection;
+namespace DistrictWeb\SphinxBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
